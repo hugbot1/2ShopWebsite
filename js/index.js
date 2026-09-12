03239118316d3d7892be56ee402b66c734fb2534
handle <132775034+hugbot1@users.noreply.github.com>
2026-09-12 00:06:54 -0500

    dispatch hotfix

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $(document).on("click", "a[data-ajax]", function (event) {
+    $(document).on("click", "a[data-dispatch]", function (event) {
         event.preventDefault();
 
         Dispatcher.navigate($(this).attr("href"));
