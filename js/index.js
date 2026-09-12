2b71458f0ab3343c8e99e6674ada93b41a4dcdc8
handle <132775034+hugbot1@users.noreply.github.com>
2026-09-11 20:28:01 -0500

    add note

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// nothing yet...
+$(document).ready(function () {
+
+    $(document).on("click", "a", function (event) {
+        event.preventDefault();
+
+        Dispatcher.navigate($(this).attr("href"));
+    });
+
+    window.onpopstate = function () {
+        Dispatcher.dispatchPage(window.location.pathname);
+    };
+
+    Dispatcher.start();
+});
